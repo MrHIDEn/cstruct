a9b62fb92707d5c1db0c044255492b1d7bad4ea9
@Marek Krzyżowski <marek.krzyzowski@evatronix.com>
2019-02-10 13:39:24 +0100

    readme

--- README.md
+++ README.md
@@ -10,26 +10,25 @@ $ npm i c_struct
 ### Examples - Level 1
 ```javascript
 const { readBufferLE } = require('c_struct');
-```
-```javascript
-let buffer = Buffer.from([1, 255, 33, 0, 0, 1]);
-let struct = ['u8', 'i8', 'u16', 'u16'];
-let [arr, offset] = readBufferLE(buffer, struct);
+let buffer, arr, obj, offset, struct;
+
+buffer = Buffer.from([1, 255, 33, 0, 0, 1]);
+struct = ['u8', 'i8', 'u16', 'u16'];
+[arr, offset] = readBufferLE(buffer, struct);
 console.log(JSON.stringify(arr)); // [1,-1,33,256]
 console.log(offset); // 6
-```
-```javascript
-let buffer = Buffer.from([1, 255, 33, 0, 0, 1]);
-let struct = { first: ['u8', 'i8'], second: ['u16', 'u16'] };
-let [obj, offset] = readBufferLE(buffer, struct);
+
+buffer = Buffer.from([1, 255, 33, 0, 0, 1]);
+struct = { first: ['u8', 'i8'], second: ['u16', 'u16'] };
+[obj, offset] = readBufferLE(buffer, struct);
 console.log(JSON.stringify(obj)); // {"first":[1,-1],"second":[33,256]}
 console.log(offset); // 6
-```
-```javascript
-let buffer = Buffer.from('a4709d3f c3f54840 c3f5c840 48656c6c6f'.replace(/ /g, ''), 'hex'); //{1.23, 3.14, 6.28, 'Hello'}
-let struct = { a: { b: 'f', c: ['f', 'f', 's5'] } };
-let [obj, offset] = readBufferLE(buffer, struct);
+
+buffer = Buffer.from('a4709d3f c3f54840 c3f5c840 48656c6c6f'.replace(/ /g, ''), 'hex'); //{1.23, 3.14, 6.28, 'Hello'}
+struct = { a: { b: 'f', c: ['f', 'f', 's5'] } };
+[obj, offset] = readBufferLE(buffer, struct);
 console.log(JSON.stringify(obj)); // {"a":{"b":1.2300000190734863,"c":[3.140000104904175,6.28000020980835,"Hello"]}}
+console.log(offset); // 17
 ```
 ### Requirements
 Node >= 8.14.*
@@ -39,19 +38,26 @@ Node >= 8.14.*
 ### Examples - Level 1, part 2
 ```javascript
 const { readBufferLE, makeBufferLE, writeBufferLE, } = require('c_struct');
-```
-```javascript
-let buffer = Buffer.from('ae47e17a14aef33f 1f85eb51b81e0940 1f85eb51b81e1940'.replace(/ /g, ''), 'hex'); //{1.23, 3.14, 6.28}
-console.log(buffer.toString('hex'));
-let struct = {
+let buffer, arr, obj, offset, struct;
+
+buffer = Buffer.from('ae47e17a14aef33f 1f85eb51b81e0940 1f85eb51b81e1940'.replace(/ /g, ''), 'hex'); //{1.23, 3.14, 6.28}
+struct = {
     a: {
         b: 'd',
-        c: [ 'd', 'd' ]
+        c: ['d', 'd']
     }
 };
-let [obj, offset] = readBufferLE(buffer, struct);
+[obj, offset] = readBufferLE(buffer, struct);
 console.log(JSON.stringify(obj)); // {"a":{"b":1.23,"c":[3.14,6.28]}}
-console.log(offset); // 17
+console.log(offset); // 24
+
+buffer = Buffer.alloc(2 * 8);
+console.log(buffer.toString('hex').match(/\w{1,16}/g).join(' ')); // 0000000000000000 0000000000000000
+struct = ['d', 'd'];
+arr = [1.23, 3.14];
+offset = writeBufferLE(buffer, struct, arr);
+console.log(buffer.toString('hex').match(/\w{1,16}/g).join(' ')); // ae47e17a14aef33f 1f85eb51b81e0940
+console.log(offset); // 16
 ```
 ```javascript
 ```
--- test/manual.js
+++ test/manual.js
@@ -1,39 +1,30 @@
 const { parseStruct, readBufferLE, makeBufferLE, writeBufferLE, } = require('../index');
 
 let b, o, s, t, r, x;
+let buffer, arr, obj, offset, struct;
 
-let buffer = Buffer.allocUnsafe(2 * 8);
-console.log(buffer.toString('hex').match(/\w{1,16}/g).join(' '));
-let struct = ['d', 'd'];
-let arr = [1.23, 3.14];
-let offset = writeBufferLE(buffer, arr);
-console.log(buffer.toString('hex').match(/\w{1,16}/g).join(' '));
-console.log(offset); // 17
+buffer = Buffer.from('ae47e17a14aef33f 1f85eb51b81e0940 1f85eb51b81e1940'.replace(/ /g, ''), 'hex'); //{1.23, 3.14, 6.28}
+struct = {
+    a: {
+        b: 'd',
+        c: ['d', 'd']
+    }
+};
+[obj, offset] = readBufferLE(buffer, struct);
+console.log(JSON.stringify(obj)); // {"a":{"b":1.23,"c":[3.14,6.28]}}
+console.log(offset); // 24
 
-// let [buff, off] = makeBufferLE({ a: { b: 'd', c: ['d', 'd'] } }, { a: { b: 1.23, c: [3.14, 6.28] } });
-// console.log(buff.toString('hex').match(/\w{1,16}/g).join(' '));
-// let buffer = Buffer.from('ae47e17a14aef33f 1f85eb51b81e0940 1f85eb51b81e1940'.replace(/ /g, ''), 'hex'); //{1.23, 3.14, 6.28}
-// console.log(buffer.toString('hex'));
-// let struct = {
-//     a: {
-//         b: 'd',
-//         c: [
-//             'd', 'd'
-//         ]
-//     }
-// };
-// let [obj, offset] = readBufferLE(buffer, struct);
-// console.log(JSON.stringify(obj, null, 2));
-// // {
-// //     "a": {
-// //     "b": 1.23,
-// //         "c": [
-// //             3.14,
-// //             6.28
-// //         ]
-// //     }
-// // }
-// console.log(offset); // 17
+buffer = Buffer.alloc(2 * 8);
+console.log(buffer.toString('hex').match(/\w{1,16}/g).join(' ')); // 0000000000000000 0000000000000000
+struct = ['d', 'd'];
+arr = [1.23, 3.14];
+offset = writeBufferLE(buffer, struct, arr);
+console.log(buffer.toString('hex').match(/\w{1,16}/g).join(' ')); // ae47e17a14aef33f 1f85eb51b81e0940
+console.log(offset); // 16
+
+
+
+
 
 
 
@@ -41,28 +32,28 @@ console.log(offset); // 17
 //let h = '0b 0c00 0d000000 f5 f4ff f3ffffff 0000a841 0000000000003640 6162630000'.replace(/ /g, '');
 //b = Buffer.from(h, 'hex');
 
-const le = {
-    u8(v) { let b = Buffer.allocUnsafe(1); b.writeUInt8(v, 0); return b; },
-    u16(v) { let b = Buffer.allocUnsafe(2); b.writeUInt16LE(v, 0); return b; },
-    u32(v) { let b = Buffer.allocUnsafe(4); b.writeUInt32LE(v, 0); return b; },
-    i8(v) { let b = Buffer.allocUnsafe(1); b.writeInt8(v, 0); return b; },
-    i16(v) { let b = Buffer.allocUnsafe(2); b.writeInt16LE(v, 0); return b; },
-    i32(v) { let b = Buffer.allocUnsafe(4); b.writeInt32LE(v, 0); return b; },
-    f(v) { let b = Buffer.allocUnsafe(4); b.writeFloatLE(v, 0); return b; },
-    d(v) { let b = Buffer.allocUnsafe(8); b.writeDoubleLE(v, 0); return b; },
-    s(v, s) { let b = Buffer.allocUnsafe(s); b.write(v.padEnd(s, '\0'), 0, s); return b; },
-}
-const be = {
-    u8(v) { let b = Buffer.allocUnsafe(1); b.writeUInt8(v, 0); return b; },
-    u16(v) { let b = Buffer.allocUnsafe(2); b.writeUInt16BE(v, 0); return b; },
-    u32(v) { let b = Buffer.allocUnsafe(4); b.writeUInt32BE(v, 0); return b; },
-    i8(v) { let b = Buffer.allocUnsafe(1); b.writeInt8(v, 0); return b; },
-    i16(v) { let b = Buffer.allocUnsafe(2); b.writeInt16BE(v, 0); return b; },
-    i32(v) { let b = Buffer.allocUnsafe(4); b.writeInt32BE(v, 0); return b; },
-    f(v) { let b = Buffer.allocUnsafe(4); b.writeFloatBE(v, 0); return b; },
-    d(v) { let b = Buffer.allocUnsafe(8); b.writeDoubleBE(v, 0); return b; },
-    s(v, s) { let b = Buffer.allocUnsafe(s); b.write(v.padEnd(s, '\0'), 0, s); return b; },
-}
+// const le = {
+//     u8(v) { let b = Buffer.allocUnsafe(1); b.writeUInt8(v, 0); return b; },
+//     u16(v) { let b = Buffer.allocUnsafe(2); b.writeUInt16LE(v, 0); return b; },
+//     u32(v) { let b = Buffer.allocUnsafe(4); b.writeUInt32LE(v, 0); return b; },
+//     i8(v) { let b = Buffer.allocUnsafe(1); b.writeInt8(v, 0); return b; },
+//     i16(v) { let b = Buffer.allocUnsafe(2); b.writeInt16LE(v, 0); return b; },
+//     i32(v) { let b = Buffer.allocUnsafe(4); b.writeInt32LE(v, 0); return b; },
+//     f(v) { let b = Buffer.allocUnsafe(4); b.writeFloatLE(v, 0); return b; },
+//     d(v) { let b = Buffer.allocUnsafe(8); b.writeDoubleLE(v, 0); return b; },
+//     s(v, s) { let b = Buffer.allocUnsafe(s); b.write(v.padEnd(s, '\0'), 0, s); return b; },
+// }
+// const be = {
+//     u8(v) { let b = Buffer.allocUnsafe(1); b.writeUInt8(v, 0); return b; },
+//     u16(v) { let b = Buffer.allocUnsafe(2); b.writeUInt16BE(v, 0); return b; },
+//     u32(v) { let b = Buffer.allocUnsafe(4); b.writeUInt32BE(v, 0); return b; },
+//     i8(v) { let b = Buffer.allocUnsafe(1); b.writeInt8(v, 0); return b; },
+//     i16(v) { let b = Buffer.allocUnsafe(2); b.writeInt16BE(v, 0); return b; },
+//     i32(v) { let b = Buffer.allocUnsafe(4); b.writeInt32BE(v, 0); return b; },
+//     f(v) { let b = Buffer.allocUnsafe(4); b.writeFloatBE(v, 0); return b; },
+//     d(v) { let b = Buffer.allocUnsafe(8); b.writeDoubleBE(v, 0); return b; },
+//     s(v, s) { let b = Buffer.allocUnsafe(s); b.write(v.padEnd(s, '\0'), 0, s); return b; },
+// }
 
 // console.log(le.s('abc', 10));
 // console.log(be.s('abc', 10));
